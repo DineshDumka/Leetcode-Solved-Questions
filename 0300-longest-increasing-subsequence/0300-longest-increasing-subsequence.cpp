f8216f8a1441c6d79efa9b8dcb08d663bc5621af
DineshDumka <136875691+DineshDumka@users.noreply.github.com>
2025-05-15 11:29:16 +0530

    Time: 4 ms (77.95%), Space: 14 MB (94.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    int rec(int i, vector<int>& arr, vector<int>& dp) {
-        if (dp[i] != -1) return dp[i];
-
-        int ans = 1;
-
-        for (int prev = 0; prev < i; ++prev) {
-            if (arr[i] > arr[prev]) {
-                ans = max(ans, rec(prev, arr, dp) + 1);
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>ans;
+        for(auto x : nums){
+            if(ans.empty()){
+                ans.push_back(x);
+            }else if(x > ans.back()){
+                ans.push_back(x);
+            }else{
+                auto lb = lower_bound(ans.begin() ,ans.end() , x) ;
+                *lb = x;
             }
         }
-        return dp[i] = ans;
-    }
-
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        int res = 0;
-
-        for (int i = 0; i < n; ++i) {
-            res = max(res, rec(i, nums, dp));
-        }
-        return res;
+        return ans.size();
     }
 };
