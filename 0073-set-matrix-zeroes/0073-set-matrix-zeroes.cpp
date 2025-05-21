f5ebb31ca973f6fffae98b8f8b40d0e54eb8fa97
DineshDumka <136875691+DineshDumka@users.noreply.github.com>
2025-05-21 07:23:36 +0530

    Time: 0 ms (100%), Space: 18.5 MB (95.54%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,21 +2,41 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& arr) {
         int n = arr.size() , m = arr[0].size();
-        vector<int>r(n,1), c(m, 1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]==0){
-                    r[i] = 0;
-                    c[j] = 0;
+        int fr= 1, fc = 1;
+        if(arr[0][0] == 0){
+            fr= 0; fc= 0;
+        }
+
+        for(int i=1;i<n;i++){
+            if(arr[i][0]==0)fc = 0;
+        }
+        for(int i=1;i<m;i++){
+            if(arr[0][i]==0)fr = 0;
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(arr[i][j] == 0){
+                    arr[i][0] = 0;
+                    arr[0][j] = 0;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(r[i]==0 || c[j]==0){
-                    arr[i][j] = 0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(arr[i][0]==0 || arr[0][j] == 0){
+                    arr[i][j]=0;
                 }
             }
+        }
+        for(int i = 0;i<n;i++){
+             if(fc == 0)   arr[i][0] = 0;          
+         }   
+        for(int j=0;j<m;j++){
+            if(fr == 0) arr[0][j] = 0;
+        }
+        if(fr ==0 || fc==0){
+            arr[0][0] = 0;
         }
        
     }
