87549c174990b354a6a0560e3983d85e45435608
DineshDumka <136875691+DineshDumka@users.noreply.github.com>
2025-08-09 19:33:43 +0530

    Time: 0 ms (100%), Space: 7.9 MB (52.81%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n>0 && not (n & n-1);
+        if(n<=0) return 0;
+        if(n == INT_MIN) return true;
+        return !(n&(n-1));
     }
 };
